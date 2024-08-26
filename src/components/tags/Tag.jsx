07ae56b6bd2b855c65520f844cb6bcac56a88032
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-26 15:17:54 +0600

    Tags and Tag component

---
+++ src/components/tags/Tag.jsx
@@ -0,0 +1,9 @@
+export default function Tag() {
+  return (
+    <div>
+      <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+        react
+      </div>
+    </div>
+  );
+}
