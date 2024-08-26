10ed50a8e01e2bf7eb7498f44fbf58c0ad7f0a10
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-26 16:55:05 +0600

    rotuer applied in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
+import Video from "./pages/Video";
 export default function App() {
   return (
-    <div>
-      <Home />
-      {/* <Video/> */}
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<Video />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
