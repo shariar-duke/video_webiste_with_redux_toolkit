5c026f0486fa32b979dd77b5f89a035cfe8d1fd9
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-26 16:56:25 +0600

    header and footer removed form Home and Video.jsx

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,13 +1,11 @@
 import Player from "../components/description/Player";
 import VideoDescription from "../components/description/VideoDescription";
-import Footer from "../components/Footer";
 import RelatedVideoList from "../components/list/RelatedVideoList";
-import Navbar from "../components/navbar/Navbar";
 
 export default function Video() {
   return (
     <>
-      <Navbar />
+    
       <section className="pt-6 pb-20">
         <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
           <div className="grid grid-cols-3 gap-2 lg:gap-8">
@@ -20,7 +18,7 @@ export default function Video() {
           </div>
         </div>
       </section>
-      <Footer />
+
     </>
   );
 }
