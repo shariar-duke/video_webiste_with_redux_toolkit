45f362e8a93c86495febcfc772cc5fd15cce1c87
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-26 16:15:23 +0600

    image problem solved

--- src/components/description/LikeUnlike.jsx
+++ src/components/description/LikeUnlike.jsx
@@ -3,13 +3,13 @@ export default function LikeUnlike() {
     <div className="flex gap-10 w-48">
       <div className="flex gap-1">
         <div className="shrink-0">
-          <img className="w-5 block" src="./assets/like.svg" alt="Like" />
+          <img className="w-5 block" src="/like.svg" alt="Like" />
         </div>
         <div className="text-sm leading-[1.7142857] text-slate-600">100K</div>
       </div>
       <div className="flex gap-1">
         <div className="shrink-0">
-          <img className="w-5 block" src="./assets/unlike.svg" alt="Unlike" />
+          <img className="w-5 block" src="/unlike.svg" alt="Unlike" />
         </div>
         <div className="text-sm leading-[1.7142857] text-slate-600">100K</div>
       </div>
