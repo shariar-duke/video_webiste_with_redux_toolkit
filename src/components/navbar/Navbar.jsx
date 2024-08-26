2499709b13e14de6161b71088b8f8389c17e1ff0
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-26 15:11:25 +0600

    Navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import Search from "./Search";
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-100 shadow-md">
+      <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
+        <a href="/">
+          <img className="h-10" src="/lws.svg" alt="Learn with Sumit" />
+        </a>
+        <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
+          <Search />
+          <img
+            className="inline h-4 cursor-pointer"
+            src="/search.svg"
+            alt="Search"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
