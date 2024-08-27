36a3a25bc21fad2048fbd5cdf2e1183c3e8cb619
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-27 13:43:15 +0600

    data added dynamcially in VideoDescription.jsx

--- src/components/description/VideoDescription.jsx
+++ src/components/description/VideoDescription.jsx
@@ -1,31 +1,23 @@
-
-import LikeUnlike from './LikeUnlike'
-export default function VideoDescription() {
+/* eslint-disable react/prop-types */
+import LikeUnlike from "./LikeUnlike";
+export default function VideoDescription({ video }) {
+  const { title, date, description } = video;
   return (
     <div>
-    <h1
-        className="text-lg font-semibold tracking-tight text-slate-800"
-    >
-        Some video title
-    </h1>
-    <div
-        className="pb-4 flex items-center space-between border-b"
-    >
-        <h2
-            className="text-sm leading-[1.7142857] text-slate-600 w-full"
-        >
-            Uploaded on 23 Nov 2022
+      <h1 className="text-lg font-semibold tracking-tight text-slate-800">
+        {title}
+      </h1>
+      <div className="pb-4 flex items-center space-between border-b">
+        <h2 className="text-sm leading-[1.7142857] text-slate-600 w-full">
+          {date}
         </h2>
 
- <LikeUnlike/>
-        
-    </div>
+        <LikeUnlike />
+      </div>
 
-    <div
-        className="mt-4 text-sm text-[#334155] dark:text-slate-400"
-    >
-        Some video description here
+      <div className="mt-4 text-sm text-[#334155] dark:text-slate-400">
+        {description}
+      </div>
     </div>
-</div>
-  )
+  );
 }
