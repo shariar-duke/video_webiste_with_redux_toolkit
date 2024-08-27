c22644165f1d5bed7fd106e56beb13559c32709d
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-27 10:16:26 +0600

    wrapped with store in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { Provider } from "react-redux";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { store } from "./app/store.js";
 import Footer from "./components/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos/:videoId" element={<Video />} />
-      </Routes>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos/:videoId" element={<Video />} />
+        </Routes>
 
-      <Footer />
-    </Router>
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
