5a4906c54bad237a84e305cc3e668377ece7f5bc
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-27 21:47:06 +0600

    action dispatch form Tag.jsx to select and unselect tag

--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,9 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useDispatch, useSelector } from "react-redux";
+import { tagRemoved, tagSelected } from "../../features/filter/filterSlice";
+
 export default function Tag({tag}) {
+
+  const {title} = tag
+  const dispatch = useDispatch()
+  // get the state 
+  const {tags:selectedTags} = useSelector(state => state.filters)
+  console.log("The tags are", selectedTags)
+  const isSelected = selectedTags.includes(title)
+  const handleSelect =()=> 
+  {
+    if(isSelected) 
+    {
+      dispatch(tagRemoved(title))
+    }
+    else 
+    {
+      dispatch(tagSelected(title))
+    }
+  }
+
+  const style = isSelected ? "bg-blue-600 text-white px-4 py-1 rounded-full coursor-pointer" : "bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
   return (
     <div>
-      <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
-       {tag?.title}
+      <div onClick={handleSelect} className={style}>
+       {title}
       </div>
     </div>
   );
