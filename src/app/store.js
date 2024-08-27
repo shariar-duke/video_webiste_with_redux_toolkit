d0313a8e04d928cec5650dfad2668474f25214cf
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-27 09:46:47 +0600

    store created

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import videoReducer from "../features/videos/videosSlice";
+
+export const store = configureStore({
+    reducer: {
+        videos: videoReducer
+    }
+})
