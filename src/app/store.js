b3d37ac499107f180c6a3e8cf850bf08f83ab7ce
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-27 13:01:52 +0600

    singleVideo slice added in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tagReducer from "../features/tags/tagsSlice";
+import singleVideoReducer from "../features/video/videoSlice";
 import videoReducer from "../features/videos/videosSlice";
 
 export const store = configureStore({
     reducer: {
         videos: videoReducer,
-        tags: tagReducer
+        tags: tagReducer,
+        video: singleVideoReducer
     }
 })
