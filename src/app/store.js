6bd3e802707c9b6a74e6943f80f67341a2dd3ba6
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-27 11:34:31 +0600

    tagReducer added in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import tagReducer from "../features/tags/tagsSlice";
 import videoReducer from "../features/videos/videosSlice";
 
 export const store = configureStore({
     reducer: {
-        videos: videoReducer
+        videos: videoReducer,
+        tags: tagReducer
     }
 })
